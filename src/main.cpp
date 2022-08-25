f1f9ba57b768f2dad5aac30b2dbbe6d0e6d18c41
jgrosse01 <jgrosse@carroll.edu>
2022-08-24 18:35:24 -0600

    Initial Commit (missing __ms_vsnprintf library)

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * Jake Grosse
+ * CS485: Computer Graphics
+ * 29 August 2022
+ *
+ * We're going to make random color noise on a GL window! WOOHOO!
+ */
+
+#include "../include/local/GLFWwindowbuilder.h"
+
+int main() {
+    run();
+    return 0;
+}
